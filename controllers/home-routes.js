e397779ab9bdf7757b49b7ec8c873ae9b854c792
Matthew Shafransky <matt@Matthews-MacBook-Pro.local>
2021-06-29 21:17:11 -0400

    added more routes

---
+++ controllers/home-routes.js
@@ -0,0 +1,76 @@
+const router = require('express').Router();
+const { Biometrics, User } = require('../models');
+const withAuth = require('../utils/auth');
+
+router.get('/', async (req, res) => {
+  try {
+    const biometricData = await Biometrics.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
+    });
+
+    const biometrics = biometricData.map((biometrics) => biometrics.get({ plain: true }));
+
+    res.render('homepage', { 
+      biometrics, 
+      logged_in: req.session.logged_in 
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/biometrics/:id', async (req, res) => {
+  try {
+    const biometricData = await Biometrics.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
+    });
+
+    const biometrics = biometricData.get({ plain: true });
+
+    res.render('biometrics', {
+      ...biometric,
+      logged_in: req.session.logged_in
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/profile', withAuth, async (req, res) => {
+  try {
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
+      include: [{ model: Biometrics }],
+    });
+
+    const user = userData.get({ plain: true });
+
+    res.render('profile', {
+      ...user,
+      logged_in: true
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/login', (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect('/profile');
+    return;
+  }
+
+  res.render('login');
+});
+
+module.exports = router;
