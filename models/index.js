d3008b2d29b870045e7c55fcc7c990d3cfbde71c
Your Name <you@example.com>
2021-07-06 21:16:19 -0400

    hotfixes

--- models/index.js
+++ models/index.js
@@ -1,4 +1,4 @@
-const User = require('./User');
+const User = require('./user');
 const Biometrics = require('./biometrics');
 const Exercise = require('./exercises');
 const Nutrition = require('./nutrition')
